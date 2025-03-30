7ccc9b49f321d8b06654a205aaa2ddf35ee9adc8
Anmitchell <avarynmitchell@gmail.com>
2025-03-30 16:15:29 -0400

    latest version of neovim config

--- lua/editor-settings/keymaps.lua
+++ lua/editor-settings/keymaps.lua
@@ -6,3 +6,15 @@ local keymap = vim.keymap
 -- Settings for keys
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights"})
+
+--Window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split window equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "close current split window" })
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
